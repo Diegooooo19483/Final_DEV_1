1835d805c8750f44a7db2e66f563c136fc6c9290
Sala 3 <sala3@FI-LabSistemas.ucatolica.loc>
2025-12-04 10:21:29 -0500

    ----------------------------- stilo

--- database.py
+++ database.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-from models import Base
+
 DATABASE_URL = "sqlite:///./sigmotoa.db"
 
 engine = create_engine(
@@ -8,8 +8,8 @@ engine = create_engine(
 )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
 Base = declarative_base()
 
 
-Base.metadata.create_all(bind=engine)
+
+
